7864c9dc89a86b0fab17302f97aedf5abd67728f
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-12-03 22:34:50 +0100

    remove single task

--- src/app/components/Task.tsx
+++ src/app/components/Task.tsx
@@ -2,10 +2,12 @@ import { TaskType } from "../types/matrixTypes";
 import DropArea from "./DropArea";
 import { useMatrixContext } from "../context/matrixContext";
 import { useMatrix } from "../hooks/useMatrix";
+import { useRemoveTask } from "../hooks/useRemoveTask";
 
 const Task = ({ positionTaskToMove, quarterTitle }: TaskType) => {
   const { matrix, setActiveTask } = useMatrixContext();
   const { onDrop } = useMatrix();
+  const { removeTask } = useRemoveTask();
 
   const matrixQuarter = matrix.find(
     (quarter) => quarter.title === quarterTitle
@@ -24,6 +26,9 @@ const Task = ({ positionTaskToMove, quarterTitle }: TaskType) => {
         className="dnd-task"
       >
         <p>{renderedTask?.id}</p>
+        <button onClick={() => removeTask(quarterTitle, positionTaskToMove)}>
+          delete
+        </button>
       </div>
       <DropArea onDrop={() => onDrop(quarterTitle, positionTaskToMove + 1)} />
     </>
