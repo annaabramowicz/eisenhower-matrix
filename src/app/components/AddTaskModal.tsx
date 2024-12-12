9a38b4b900a8cf29c3b3016a5f9a8bc2962cc099
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-12-12 14:33:48 +0100

    AddTaskModal with custom styles

--- src/app/components/AddTaskModal.tsx
+++ src/app/components/AddTaskModal.tsx
@@ -1,9 +1,13 @@
-const AddTaskModal = ({ onClose }) => {
-  console.log("hej");
+const AddTaskModal = ({ onClose }: { onClose: () => void }) => {
   return (
-    <div className="modal">
-      <div>Im a modal dialog</div>
-      <button onClick={onClose}>Close</button>
+    <div className="absolute top-0 left-0 z-[999] w-screen h-screen bg-black/30 ">
+      <dialog
+        open
+        className="flex justify-evenly flex-col items-center absolute inset-x-1/2 translate-x-[-50%] translate-y-1/2 h-[50vh] w-[50vh] rounded-md"
+      >
+        <div>Im a modal dialog</div>
+        <button onClick={onClose}>Close</button>
+      </dialog>
     </div>
   );
 };
