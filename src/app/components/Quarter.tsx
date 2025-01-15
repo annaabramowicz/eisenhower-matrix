0c615044262362e046aa35576a2c5aea2a08a5e0
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-01-15 11:18:13 +0100

    fixed quarter key error

--- src/app/components/Quarter.tsx
+++ src/app/components/Quarter.tsx
@@ -14,7 +14,13 @@ const Quarter = ({ quarterTasks, quarterTitle }: QuarterTasksProps) => {
     <div className="flex flex-col bg-base-300 rounded-md text-center mx-auto w-full max-h-full p-4">
       <h2 className="mb-0">{quarterTitle}</h2>
       {quarterTasks.tasks.map((task, positionTaskToMove) => {
-        return <Task key={task.id} positionTaskToMove={positionTaskToMove} quarterTitle={quarterTitle} />;
+        return (
+          <Task
+            key={`${task.id}-${positionTaskToMove}`}
+            positionTaskToMove={positionTaskToMove}
+            quarterTitle={quarterTitle}
+          />
+        );
       })}
       <DropArea styleLastElement={"grow"} onDrop={() => moveTask(quarterTitle)} />
     </div>
