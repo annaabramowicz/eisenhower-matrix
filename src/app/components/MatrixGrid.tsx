0538df72625db10082a62bbed0d86d01cf131c2e
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-01-22 12:11:32 +0100

    skeleton and draft of suspense

--- src/app/components/MatrixGrid.tsx
+++ src/app/components/MatrixGrid.tsx
@@ -1,21 +1,29 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { lazy, Suspense, useCallback, useEffect, useState } from "react";
 import AddTaskButton from "./AddTaskButton";
 import Matrix from "./Matrix";
 import MatrixSection from "./MatrixSection";
 import axios from "axios";
 import { initialMatrix, useMatrixContext } from "../context/matrixContext";
 import { mapperTaskApiData } from "../helpers/mapperTaskApiData";
+import Skeleton from "./Skeleton";
+
+// 2
+// const Matrix = lazy(() => import("./Matrix"));
 
 const MatrixGrid = () => {
   const { setMatrix } = useMatrixContext();
+  // 1
+  const [loading, setLoading] = useState(true);
 
   const getMatrixDB = useCallback(async () => {
     try {
       const { data } = await axios.get("/api/matrix");
       const newMatrix = mapperTaskApiData(data, initialMatrix);
       setMatrix(newMatrix);
+      // 1
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -32,7 +40,31 @@ const MatrixGrid = () => {
       <MatrixSection>NOT URGENT</MatrixSection>
       <MatrixSection className="[writing-mode:vertical-lr] [transform:rotate(180deg)]">IMPORTANT</MatrixSection>
       <div className="row-span-2 col-span-2 flex items-center justify-center  text-white font-bold rounded shadow">
-        <Matrix />
+        {/* 2 */}
+        {/* <Suspense
+          fallback={
+            // <Skeleton />
+            <>
+              <div style={{ background: "red" }}>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+                <h1>Ładowanie...</h1>
+              </div>
+            </>
+          }
+        >
+          <Matrix />
+        </Suspense> */}
+        {/* 1 */}
+        {loading ? <Skeleton /> : <Matrix />}
       </div>
       <MatrixSection className="[writing-mode:vertical-lr] [transform:rotate(180deg)]">NOT IMPORTANT</MatrixSection>
     </div>
---
+++ src/app/components/Skeleton.tsx
@@ -0,0 +1,12 @@
+const Skeleton = () => {
+  return (
+    <div className="w-full h-full grid grid-cols-2 grid-rows-2 gap-2">
+      <div className="skeleton rounded-md"></div>
+      <div className="skeleton rounded-md"></div>
+      <div className="skeleton rounded-md"></div>
+      <div className="skeleton rounded-md"></div>
+    </div>
+  );
+};
+
+export default Skeleton;
