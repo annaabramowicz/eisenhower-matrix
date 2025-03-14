37aa40442631c944cbada9e9b1aec81a8d9b3349
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-03-14 13:55:34 +0100

    deleted unused code

--- src/app/helpers/idCreator.tsx
+++
@@ -1,1 +0,0 @@
-export const getNewId = () => `${new Date().getTime()}`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,9 @@ import { mapperTaskApiData } from "./helpers/mapperTaskApiData";
 import connectDB from "./lib/connectDB";
 import Task from "./model/task";
 
-// const MatrixSchema = z.array(
-//   z.object({
-//     quarterActiveTask: z.string(),
-//     title: z.string(),
-//   })
-// );
 export default async function Home() {
   await connectDB();
   const data = await Task.find({});
-  // MatrixSchema.parse(data);
   const newMatrix = mapperTaskApiData(data, initialMatrix);
 
   return (
